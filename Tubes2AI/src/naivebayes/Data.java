717873fea7b01435ead77dc0a9b56b092e368382
alsoncahyadi <alson.cahyadi@gmail.com>
2016-11-21 16:27:06 +0700

    NB now working

--- Tubes2AI/src/naivebayes/Data.java
+++ Tubes2AI/src/naivebayes/Data.java
@@ -10,5 +10,57 @@ package naivebayes;
  * @author alson
  */
 public class Data {
+    private int column;      //atribut
+    private int value;        //nilai dari atribut
+    private int result;      //kelas
+    private double freq;         //frekuensi nilai pada kolom tersebut
+    private double total;        //pembagi per kolom per result
+
+    public int getcolumn() {
+        return column;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public double getfreq() {
+        return freq;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setcolumn(int column) {
+        this.column = column;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public void setfreq(double freq) {
+        this.freq = freq;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
     
+    public Data(int coll, int val, int rslt, double fr){
+        column = coll;
+        value = val;
+        result = rslt;
+        freq = fr;
+        total = 0;
+    }
 }
